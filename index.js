799eacd351839d6d15e21105dc86eefaac3cf3e6
Salomon <escttsalo@gmail.com>
2021-04-22 23:59:18 -0600

    finished last two classes

--- index.js
+++ index.js
@@ -165,8 +165,22 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ class Student extends Lambdasian{
+     constructor(att){
+        super(att)
+        this.previousBackground = att.previousBackground;
+        this.className = att.className;
+        this.favSubjects = att.favSubjects;
+     }
+     listSubjects(){
+       return `Loving ${this.favSubjects}`
+     }
+     PRAssignment(subject){
+        return `${this.name} has submitted a PR for ${subject}`
+     }
+     sprintChallenge(subject){
+        return `${this.name} has begun spring challenge on ${subject}`
+     }
  }
   
   /*
